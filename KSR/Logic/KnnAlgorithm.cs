735d90dea539b809cc3603d18958729046a9c255
orzio <andrzej.orzinski@gmail.com>
2020-05-10 14:25:11 +0200

    back to previous commit

--- KSR/Logic/KnnAlgorithm.cs
+++ KSR/Logic/KnnAlgorithm.cs
@@ -12,10 +12,22 @@ namespace Logic
             var neighbours = TrainingArticles.Take(neighboursAmount).ToList(); //bierze k sąsiadów
 
             var result = neighbours.GroupBy(x => x.Place)
-                                   .Select(x => new { Country = x.Key, Count = x.Count(), Avg = neighbours.Where(p => p.Place == x.Key).Sum(x => x.Distance) / x.Count() })
-                                   .OrderBy(n => n.Avg).FirstOrDefault().Country;
+                                  .Select(x => new { Country = x.Key, Count = x.Count() })
+                                  .OrderByDescending(n => n.Count);
+            var results = new List<Result>();
+            foreach (var grp in result)
+            {
+                var item = new Result { Country = grp.Country, Occurence = grp.Count };
+                results.Add(item);
+            }
 
-            testArticle.AssignedPlace = result;
+            testArticle.AssignedPlace = results.FirstOrDefault().Country;
+        }
+
+        class Result
+        {
+            public string Country { get; set; }
+            public int Occurence { get; set; }
         }
     }
 }
